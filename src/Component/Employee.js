aa1d2e85fbb586b53ea6dbe8ce795452c0cb1183
arpita deb <arpitadeb1990@gmail.com>
2019-04-08 12:12:42 -0600

    initial of scheduling

---
+++ src/Component/Employee.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+class Employee extends Component {
+    render(){
+        return(
+            <div>
+
+                <div>
+                    User 1
+                </div>
+
+                <div>
+                    User 2
+                </div>
+
+                <div>
+                    User 3
+                </div>
+
+            </div>
+        );
+    }
+
+}
+
+export default Employee;
---
+++ src/Component/Schedule.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class Schedule extends Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    Monday: 9am - 5pm
+                </div>
+                <div>
+                    Tuesday: 10am - 6pm
+                </div>
+                <div>
+                    Wednesday: 11am - 6pm
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Schedule;
